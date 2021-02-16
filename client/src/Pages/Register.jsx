f50a5bd3ac183c5a0304b113e28b84bdf63deb81
William Payne <payne.j.will@gmail.com>
2021-02-15 22:35:19 -0600

    fix routes

--- client/src/Pages/Register.jsx
+++ client/src/Pages/Register.jsx
@@ -6,7 +6,6 @@ import { register } from "../Actions/Auth";
 import { makeStyles } from "@material-ui/core";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
-import Link from "@material-ui/core/Link";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import { Paper } from "@material-ui/core";
